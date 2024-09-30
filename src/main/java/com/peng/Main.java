16ba1435360fc848703d28461e371d42985816ab
spengju <2658111307@qq.com>
2024-09-30 21:24:17 +0800

    first commit

---
+++ src/main/java/com/peng/Main.java
@@ -0,0 +1,13 @@
+package com.peng;
+
+/**
+ * @Author: spengju
+ * @Slogan: Day day no bug.
+ * @Date: 2024/9/30 21:22
+ * $description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
